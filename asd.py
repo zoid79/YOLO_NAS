3b0e5f72a0bd9bf7598457fd6f58b89d57199970
zoid79 <zidwkd00@naver.com>
2024-03-27 17:40:54 +0900

    first

---
+++ asd.py
@@ -0,0 +1,16 @@
+import torch
+from super_gradients.training.losses import PPYoloELoss
+from super_gradients.training.metrics import DetectionMetrics_050
+from super_gradients.training.models.detection_models.pp_yolo_e import PPYoloEPostPredictionCallback
+from super_gradients.training import models
+print("zxc")
+print("zxc")
+print("zxc")
+print("zxc")
+a = torch.cuda.is_available()
+print(a)
+
+print("zxc")
+print("zxc")
+print("zxc")
+#%%
--- yolo_nas_custom_real_time.py
+++ yolo_nas_custom_real_time.py
@@ -1,10 +1,174 @@
-import cv2
 import torch
+from IPython.core.display_functions import clear_output
+from super_gradients.training.losses import PPYoloELoss
+from super_gradients.training.metrics import DetectionMetrics_050
+from super_gradients.training.models.detection_models.pp_yolo_e import PPYoloEPostPredictionCallback
 from super_gradients.training import models
-from super_gradients.common.object_names import Models
+from super_gradients.training.utils.callbacks import LinearEpochLRWarmup
 
-model = models.get('yolo_nas_s',num_classes=7,checkpoint_path='weights/yolo_nas_s')
+#-- GPU 설정
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
+use_cuda = torch.cuda.is_available()
+print(use_cuda)
+if use_cuda:
+  print(torch.cuda.get_device_name(0))
+#%%
+from super_gradients.training import Trainer
+CHECKPOINT_DIR = 'checkpoints'
+trainer = Trainer(experiment_name='Pokemon_yolonas_run', ckpt_root_dir=CHECKPOINT_DIR)
+#%%
+from roboflow import Roboflow
+rf = Roboflow(api_key="3NnsrtodD4CdBVrs1VJk")
+project = rf.workspace("pokmon").project("pokemon-2atdu")
+dataset = project.version(1).download("yolov5")
+#%%
+dataset_params = {
+    'data_dir':'Pokemon-1',
+    'train_images_dir':'train/images',
+    'train_labels_dir':'train/labels',
+    'val_images_dir':'valid/images',
+    'val_labels_dir':'valid/labels',
+    'test_images_dir':'test/images',
+    'test_labels_dir':'test/labels',
+    'classes': ['Bulbasaur','Charmander','Squirtle']
+}
+#%%
+from super_gradients.training import dataloaders
+from super_gradients.training.dataloaders.dataloaders import coco_detection_yolo_format_train, coco_detection_yolo_format_val
 
-model = model.to("cuda" if torch.cuda.is_available() else 'cpu')
+train_data = coco_detection_yolo_format_train(
+    dataset_params={
+        'data_dir': dataset_params['data_dir'],
+        'images_dir': dataset_params['train_images_dir'],
+        'labels_dir': dataset_params['train_labels_dir'],
+        'classes': dataset_params['classes'],
+        'show_all_warnings': True
+    },
+    dataloader_params={
+        'batch_size':16,
+        'num_workers':2
+    }
+)
 
-model.predict_webcam()
+val_data = coco_detection_yolo_format_val(
+    dataset_params={
+        'data_dir': dataset_params['data_dir'],
+        'images_dir': dataset_params['val_images_dir'],
+        'labels_dir': dataset_params['val_labels_dir'],
+        'classes': dataset_params['classes']
+    },
+    dataloader_params={
+        'batch_size':16,
+        'num_workers':2
+    }
+)
+
+
+test_data = coco_detection_yolo_format_val(
+    dataset_params={
+        'data_dir': dataset_params['data_dir'],
+        'images_dir': dataset_params['test_images_dir'],
+        'labels_dir': dataset_params['test_labels_dir'],
+        'classes': dataset_params['classes']
+    },
+    dataloader_params={
+        'batch_size':16,
+        'num_workers':2
+    }
+)
+
+clear_output()
+#%%
+train_data.dataset.transforms
+#%%
+train_data.dataset.dataset_params['transforms'][1]
+#%%
+train_data.dataset.dataset_params['transforms'][1]['DetectionRandomAffine']['degrees'] = 10.42
+#%%
+train_data.dataset.plot()
+#%%
+model = models.get('yolo_nas_l',
+                   num_classes=len(dataset_params['classes']),
+                   pretrained_weights="coco"
+                   )
+#%%
+train_params = {
+    # ENABLING SILENT MODE
+    'silent_mode': True,
+    "average_best_models":True,
+    "warmup_mode": "LinearEpochLRWarmup",  # 변경된 부분
+    "warmup_initial_lr": 1e-6,
+    "lr_warmup_epochs": 3,
+    "initial_lr": 5e-4,
+    "lr_mode": "cosine",
+    "cosine_final_lr_ratio": 0.1,
+    "optimizer": "Adam",
+    "optimizer_params": {"weight_decay": 0.0001},
+    "zero_weight_decay_on_bias_and_bn": True,
+    "ema": True,
+    "ema_params": {"decay": 0.9, "decay_type": "threshold"},
+    # 이 예제의 경우 15개의 에포크만 교육한다.
+    "max_epochs": 20,
+    "mixed_precision": True,
+    "loss": PPYoloELoss(
+        use_static_assigner=False,
+        # 참고: num_classes는 여기에 정의되어야 한다.
+        num_classes=len(dataset_params['classes']),
+        reg_max=16
+    ),
+    "valid_metrics_list": [
+        DetectionMetrics_050(
+            score_thres=0.1,
+            top_k_predictions=300,
+            # 참고: num_classes는 여기에 정의되어야 한다.
+            num_cls=len(dataset_params['classes']),
+            normalize_targets=True,
+            post_prediction_callback=PPYoloEPostPredictionCallback(
+                score_threshold=0.01,
+                nms_top_k=1000,
+                max_predictions=300,
+                nms_threshold=0.7
+            )
+        )
+    ],
+    "metric_to_watch": 'mAP@0.50'
+}
+#%%
+model.eval()
+torch.no_grad()
+trainer.train(model=model,
+              training_params=train_params,
+              train_loader=train_data,
+              valid_loader=val_data)
+#%%
+best_model = models.get('yolo_nas_l',
+                        num_classes=len(dataset_params['classes']),
+                        checkpoint_path="/content/checkpoints/Pokemon_yolonas_run/RUN_20231226_114257_787184/ckpt_best.pth")
+                        #-- ckpt_best.pth 파일이 있는 경로를 찾아서 넣어주세용
+#%%
+trainer.test(model=best_model,
+            test_loader=test_data,
+            test_metrics_list=DetectionMetrics_050(score_thres=0.1,
+                                                   top_k_predictions=300,
+                                                   num_cls=len(dataset_params['classes']),
+                                                   normalize_targets=True,
+                                                   post_prediction_callback=PPYoloEPostPredictionCallback(score_threshold=0.01,
+                                                                                                          nms_top_k=1000,
+                                                                                                          max_predictions=300,
+                                                                                                          nms_threshold=0.7)
+                                                  ))
+#%%
+import locale
+def getpreferredencoding(do_setlocale = True):
+    return "UTF-8"
+locale.getpreferredencoding = getpreferredencoding
+
+#!gdown "https://drive.google.com/uc?id=1HsCBy8HU0Rqs-nb_mScRXVmLwrY1G3UQ"
+#%%
+input_video_path = f"/content/pokemon.mp4"
+output_video_path = "detections.mp4"
+
+best_model.to(device)
+best_model.predict(input_video_path, conf = 0.4).save(output_video_path)
+#%%
